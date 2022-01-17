ddc2304c6014e1e0c396d1c8d9d96f2012a138f8
Adilson Lima <adilson.monte.alto@gmail.com>
2022-01-17 09:33:49 -0300

    master:requirements.txt

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 python3 -m venv venv;
-. venv/bin/activate;
+echo "Ambiente virtual instalado"
+source venv/bin/activate;
+echo "Ambiente virtual ativado"
 pip install -r requirements.txt;
 pre-commit install;
